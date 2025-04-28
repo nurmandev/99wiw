7df12f6d6440c69a302be73c3de6623e396469f0
Asin-Junior-Honore <asinhonore823@gmail.com>
2025-04-28 10:14:28 +0100

    feat:added new games

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router.jsx";
 import { GlobalProvider } from "./context/GlobalContext.jsx";
